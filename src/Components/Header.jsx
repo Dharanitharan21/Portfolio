c02ff30095d4d921b2aeb976d3dd8389fbf8300f
Dharanitharan <dharanitharan2112@gmail.com>
2025-07-15 12:21:36 +0530

    first commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
         <header className=' fixed top-0 right-0 left-0 bg-white/90 backdrop-blur-md border-b border-gray-200 z-50 '>
             <nav className='container mx-auto px-10 py-4' >
                 <div className='flex justify-between items-center'>
-                    <div className=' text-2xl font-bold bg-gradient-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent'>Dharan</div>
+                    <div className=' text-2xl font-bold bg-gradient-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent'>Protfolio</div>
                     <div className='hidden md:flex space-x-8'>
                         <button  onClick={() => scrollToSection('about')}
                         className='flex items-center gap-2 text-gray-700 hover:text-blue-600 transition-colors'>
